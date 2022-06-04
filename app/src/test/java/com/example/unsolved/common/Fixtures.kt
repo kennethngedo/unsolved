05950704041df38a873c9c6841b6e0c945d3ad56
Kenneth Ngedo <kenneth.ngedo@gmail.com>
2022-06-04 02:11:35 +0100

    test: add fixtures to fascilate tests

---
+++ app/src/test/java/com/example/unsolved/common/Fixtures.kt
@@ -0,0 +1,34 @@
+package com.example.unsolved.common
+
+import android.content.res.Resources.NotFoundException
+import java.io.BufferedReader
+import java.io.File
+import java.io.FileInputStream
+import java.io.InputStreamReader
+import java.lang.StringBuilder
+import java.net.URL
+
+class Fixtures {
+    companion object {
+        fun getFileFromPath(obj: Any, fileName: String): File? {
+            val classLoader: ClassLoader? = obj.javaClass.classLoader
+            val resource: URL? = classLoader?.getResource(fileName)
+            return if (resource != null) File(resource.path) else null
+        }
+
+        fun getFixtureAsString(obj: Any, fileName: String): String {
+            val classLoader: ClassLoader? = obj.javaClass.classLoader
+            val resource: URL? = classLoader?.getResource(fileName)
+            return if (resource != null) {
+                val br = BufferedReader(InputStreamReader(FileInputStream(resource.path)))
+                val sb = StringBuilder()
+                var line = br.readLine()
+                while (line != null) {
+                    sb.append(line)
+                    line = br.readLine()
+                }
+                sb.toString()
+            } else ""
+        }
+    }
+}
